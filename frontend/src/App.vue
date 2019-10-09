a62e7ba88c0408d246c3be860d4ddefc5464d14b
ulissestb <ulisses.melo.nascimento@gmail.com>
2019-10-09 15:40:09 -0300

    Plugin Vuetify adicionado e primeiros passos dados no VueJS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <Header />
+    <Tabela />
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
+import Tabela from "./components/Tabela";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Tabela
+  },
+  data: () => ({
+    //
+  })
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <img src="../assets/iblue.png" />
+    <br />
+    <h2>Nkiambi's Challenge</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.header img {
+  width: 200px;
+}
+.header h2 {
+  color: #221942;
+}
+</style>
