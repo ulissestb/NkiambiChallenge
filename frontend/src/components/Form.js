3807ae96b8c019d68d42c2e190ee8b857cd6cbb4
ulissestb <ulisses.melo.nascimento@gmail.com>
2019-10-11 12:04:53 -0300

    Adicionado react-bootstrap para ajudar no layout do projeto

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { Button, FormControl, FormGroup} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import api from '../services/api'
+
+export default function Form(){
+
+    const addPlanet = async ({name, climate, terrain, films})=>{
+        await api.post("/planets/add",{name, climate, terrain, films})
+
+        console.log("adicionado")
+    }
+
+    const randomPlanet =  async () =>{
+        const randomNumber = Math.floor(Math.random() * 60)
+        await api.get(`/planets/${randomNumber}/`)
+        console.log(randomNumber)
+        window.location.reload()
+    }
+    
+    return(
+        <div className="form-list">
+        <FormGroup>
+            Nome: <FormControl type="text" placeholder="Nome do Planeta" />
+            Clima: <FormControl type="text" placeholder="Clima do Planeta"/>
+            Terreno: <FormControl type="text" placeholder="Terreno do Planeta"/>
+            Aparições: <FormControl type="text" placeholder="Aparições do planeta nos filmes"/>
+        <div className="btnn">
+            <Button  variant="success" >Adicionar</Button>
+            <Button  variant="secondary " onClick={()=> randomPlanet()}>Aleatório</Button>
+        </div>
+        </FormGroup>
+    </div>
+    )
+}
